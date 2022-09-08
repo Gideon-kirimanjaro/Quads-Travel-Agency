2e8d7a9b63ac46c595c8e0d5c12da2ee88afded7
Gideon Kirimanjaro <gideonkirimanjaro@gmail.com>
2022-09-08 08:31:37 +0300

    icons

--- components/Data/GridCardData.js
+++ components/Data/GridCardData.js
@@ -1,31 +1,31 @@
 const gridCardData = [
   {
     key: 1,
-    src: "/icons/africa.png",
+    src: "/icons/africa.PNG",
     header: "Locations",
     body: "Get to travel the world and experience new places",
     alt: "africa",
   },
   {
     key: 2,
-    src: "/icons/people.png",
+    src: "/icons/people.PNG",
     header: "People",
     body: "Explore new cultures and appreciate diversity",
     alt: "people",
   },
   {
     key: 3,
-    src: "/icons/hospitality.png",
+    src: "/icons/hospitality.PNG",
     header: "Hospitality",
     body: "We value our customers comfort creating a home away from home",
-    alt: "people",
+    alt: "hospitality",
   },
   {
     key: 4,
-    src: "/icons/commitment.png",
+    src: "/icons/commitment.PNG",
     header: "Commitment",
     body: "We are a value based company and we take pride in our services",
-    alt: "people",
+    alt: "commitment",
   },
 ];
 export default gridCardData;
