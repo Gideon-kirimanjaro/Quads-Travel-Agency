9e55362f72db707186887faec6855e2a6a8a94d4
Gideon Kirimanjaro <gideonkirimanjaro@gmail.com>
2022-09-09 13:08:07 +0300

    image aspect ratio

--- pages/careers/[id].js
+++ pages/careers/[id].js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import RoutedCareer from "../../components/Careers/RoutedCareer/RoutedCareer";
 import careersData from "../../components/Data/CareerData";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 const CareersPage = () => {
   const router = useRouter();
   const [loadCareers, setLoadCareers] = useState([]);
@@ -19,6 +20,11 @@ const CareersPage = () => {
   console.log("LOG THE CAREER", loadCareers);
   return (
     <div style={{ marginTop: "150px" }}>
+      <Head>
+        <title>Quads Travel Agency</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       {loadCareers === [] ? (
         <p>Loading careers.........</p>
       ) : (
--- pages/tours/[id].js
+++ pages/tours/[id].js
@@ -27,6 +27,7 @@ const TravelPage = () => {
       {" "}
       <Head>
         <title>Quads Travel Agency</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <TopNav />
